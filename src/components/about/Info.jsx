894ca8700730d654f3487a703b90498daa20805b
koayenay <arfan.myint.607@my.csun.edu>
2024-01-22 15:38:38 -0800

    dynamically calculate the YOE

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -1,12 +1,16 @@
 import React from "react"
 
 const Info = () => {
+  // Calculate the number of years since 2019
+  const startYear = 2019
+  const currentYear = new Date().getFullYear()
+  const experienceYears = currentYear - startYear
   return (
     <div className='about__info grid'>
       <div className='about__box'>
         <i className='bx bx-award about__icon'></i>
         <h3 className='about__title'>Experience</h3>
-        <span className='about__subtitle'>3 Years </span>
+        <span className='about__subtitle'>{experienceYears} Years </span>
       </div>
 
       <div className='about__box'>
